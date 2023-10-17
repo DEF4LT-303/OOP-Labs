8430515131651a69abaf5c10b952203009ab83c6
Ryan Rafi <ryanrafi72@gmail.com>
2023-10-17 15:38:06 +0600

    Update Login.java

--- Lab2/src/Login.java
+++ Lab2/src/Login.java
@@ -2,8 +2,10 @@ import java.sql.*;
 
 public class Login {
 
-    public Login(Connection con, String email, String password) throws Exception {
+    public Login(Connection con, Statement statement, String email, String password) throws Exception {
         String query = "SELECT * FROM users WHERE Email = ? AND Password = ?";
+
+        ResultSet details = statement.executeQuery("select * from course");
 
         PreparedStatement preparedStatement = con.prepareStatement(query);
         preparedStatement.setString(1,email);
@@ -12,14 +14,23 @@ public class Login {
         ResultSet resultSet = preparedStatement.executeQuery();
 
         if (resultSet.next()) {
-            do {
-                System.out.print(resultSet.getInt("SID") + " ");
-                System.out.print(resultSet.getString("Name") + " ");
-                System.out.print(resultSet.getString("Email") + " ");
+            System.out.println("Login Successful!\n");
+            System.out.print(resultSet.getInt("SID") + " ");
+            System.out.print(resultSet.getString("Name") + " ");
+            System.out.print(resultSet.getString("Email") + " ");
+
+            System.out.println("\nCourse details:");
+
+            while (details.next()) {
+                System.out.print(details.getString("Section") + " ");
+                System.out.print(details.getString("Time") + " ");
+                System.out.print(details.getString("Seats") + " ");
                 System.out.println();
-            } while (resultSet.next());
+            }
+            System.out.println();
+
         } else {
-            System.out.println("Invalid login credentials");
+            System.out.println("Invalid login credentials...");
         }
     }
 }
